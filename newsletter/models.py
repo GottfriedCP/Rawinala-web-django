8f6c8623245126a371eec539baace19073826b90
GottfriedCP <gottfriedcpn@gmail.com>
2018-04-15 15:57:33 +0700

    changed human-readable representation of each newsletter instance

--- newsletter/models.py
+++ newsletter/models.py
@@ -25,4 +25,4 @@ class Newsletter(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '%s %s' % (self.created_at.month, self.created_at.year)
+        return '%s %s' % (self.created_at.year, self.created_at.month)
